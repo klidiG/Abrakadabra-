3d3c411d0198f5423c7fd0f5fd5c6dc88cdbd6a7
klidiG <58671649+klidiG@users.noreply.github.com>
2019-12-14 23:12:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+//The setup function only happens once
+
+
+
+function setup() {
+	createCanvas(800, 600); //create a 500px X 500px canvas
+ background(255,204,0); //an RGB color for the canvas' background
+  //circle
+}
+
+//The draw function happens over and over again
+function draw() {
+  stroke(255) // an RGB color for the circle's border
+  fill(0); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  ellipse(mouseX, mouseY, 100, 100); // ellipse means a circular shape, mouse X means means the element will travel to where the mouse is horizontally, and mouse Y means the element will travel to where the mouse is vertically. the first number 100 represents the width (100 pixels) and the second number 100 represent the length (100 pixels) 
+}
